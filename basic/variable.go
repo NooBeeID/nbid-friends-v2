8f7bf0a4ddb4e40a7736588ec180756c11392ff9
Reyhan Jovie Dwiputra <reyhanjovie01@gmail.com>
2022-12-24 09:53:46 +0700

    initialize

---
+++ basic/variable.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+// var name string = "Noobeeid"
+// var age = 10
+
+// var (
+// 	name = "noobeeid"
+// 	age  = 10
+// )
+
+var name, age = "noobeeid", 10
+
+func main() {
+	// name, age := "noobeeid", 10
+	// user := "Reyhan"
+
+	// fmt.Println(name, age, user)
+	// text, twiceAge := get("main", age)
+	// fmt.Println("return from get", text, "age is", twiceAge)
+
+	students := []string{"noobeeid", "reyhan", "reza", "nurizko", "suhri", "ahmad"}
+
+	f := find(students)
+	isExist := f("reyhan")
+	isExistB := f("fikri")
+	fmt.Println(isExist, isExistB)
+}
+
+func get(text string, age int) (string, int) {
+	fmt.Println("Ini function get", text, age*2)
+	post(10, 20, 40, 50, 60, 10, 20, 30, 40, 50, 60, 70, 70)
+
+	return text, age * 2
+}
+
+func post(data ...int) {
+	fmt.Println("ini post")
+	var total = 0
+	for i := 0; i < len(data); i++ {
+		total += data[i]
+	}
+
+	fmt.Println(total)
+
+	coba(10, timesTo20)
+	coba(20, func(i int) int {
+		return i * 3
+	})
+	coba(30, func(i int) int {
+		return i
+	})
+}
+
+func timesTo20(i int) int {
+	return i * 20
+}
+
+func coba(num int, cb func(int) int) {
+	total := cb(num)
+	fmt.Println("total callback", total)
+}
+
+func find(students []string) func(string) bool {
+	return func(s string) bool {
+		isExist := false
+		for i := 0; i < len(students); i++ {
+			if students[i] == s {
+				isExist = true
+				break
+			}
+		}
+
+		return isExist
+	}
+}
